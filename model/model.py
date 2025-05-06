adeb63d23f5483eea2066371db56b96b4ce2dee7
sbohmann <github@yeoman.at>
2025-05-06 20:36:41 +0200

    Model / Types

--- model/model.py
+++ model/model.py
@@ -10,18 +10,23 @@ class KindOfType(Enum):
     Set = 5
     Map = 6
 
+
 class Type:
     name: str
-    is_primitive: bool
+    kind_of_type: KindOfType
     type_arguments: list['Type']
+
     @abstractmethod
     def __init__(self, name: str, type_arguments: list['Type']):
         pass
 
+
 class PrimitiveType(Type):
     def __init__(self, name: str):
         self.name = name
-        self.is_primitive = True
+        self.kind_of_type = KindOfType.Primitive
+        self.type_arguments = []
+
 
 PrimitiveType.Boolean = PrimitiveType('Boolean')
 PrimitiveType.Integer = PrimitiveType('Integer')
@@ -36,22 +41,57 @@ PrimitiveType.UInt64 = PrimitiveType('UInt64')
 PrimitiveType.Float = PrimitiveType('Float')
 PrimitiveType.String = PrimitiveType('String')
 
+
 class Field:
     name: str
     type: Type
 
-class Struct:
-    name: str
+
+class Struct(Type):
     fields: list[Field]
 
-class Enum:
+    def __init__(self, name: str, fields: list[Field]):
+        self.name = name
+        self.kind_of_type = KindOfType.Struct
+        self.fields = fields
+
+
+class Enum(Type):
     values: list[str]
+
+    def __init__(self, name: str, values: list[str]):
+        self.name = name
+        self.kind_of_type = KindOfType.Enum
+        self.values = values
+
+
+class List(Type):
+    def __init__(self, name: str, element_type: Type):
+        self.name = name
+        self.kind_of_type = KindOfType.List
+        self.type_arguments = [element_type]
+
+
+class Set(Type):
+    def __init__(self, name: str, element_type: Type):
+        self.name = name
+        self.kind_of_type = KindOfType.Set
+        self.type_arguments = [element_type]
+
+
+class Map(Type):
+    def __init__(self, name: str, key_type: Type, value_type: Type):
+        self.name = name
+        self.kind_of_type = KindOfType.Map
+        self.type_arguments = [key_type, value_type]
+
 
 class Module:
     structs: list[Struct]
     structForName: dict[str, Struct]
     enums: list[Enum]
     enumForName: dict[str, Enum]
+
 
 class Model:
     modules: list[Module]
